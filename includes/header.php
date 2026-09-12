f486953f487cec237dde6231fecc096f4f14bfc7
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-09-12 17:16:12 +0600

    added header file

---
+++ includes/header.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once __DIR__ . '/functions.php';
+
+$page_title = $page_title ?? APP_NAME;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>
+        <?= e($page_title); ?>
+    </title>
+
+    <link
+        rel="stylesheet"
+        href="<?= url('assets/css/bootstrap.min.css'); ?>"
+    >
+
+    <link
+        rel="stylesheet"
+        href="<?= url('assets/css/style.css'); ?>"
+    >
+
+</head>
+
+<body>
