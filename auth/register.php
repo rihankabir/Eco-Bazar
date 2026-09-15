fea7221560797417fd0275384bff4af24bd200e6
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-09-15 20:22:17 +0600

    added register page php codes

---
+++ auth/register.php
@@ -0,0 +1,8 @@
+<?php
+
+$page_title = 'Create Account';
+
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../includes/header.php';
+
+?>
--- includes/auth-check.php
+++ includes/auth-check.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/functions.php';
 
 if(!isset($_SESSION['user_id'])){
+    
       header('Location: /Ecomart/auth/login.php');
 
     exit;
